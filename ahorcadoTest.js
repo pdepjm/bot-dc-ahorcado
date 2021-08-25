279204b1bbdd0dc3ca6d326132928b67d7c26271
PalumboN <nahuel.palumbo@gmail.com>
2021-08-25 16:31:37 -0300

    Mejores tests

--- ahorcadoTest.js
+++ ahorcadoTest.js
@@ -2,7 +2,7 @@ const assert = require('assert')
 const { Ahorcado } = require('./ahorcado')
 
 describe('Ahorcado', () => {
-  const FRASE = 'Palabra'
+  const FRASE = '¿Una frase!'
   let ahorcado
 
   beforeEach(() => {
@@ -11,7 +11,7 @@ describe('Ahorcado', () => {
 
   describe('al iniciar', () => {
     it('frase', () =>
-      assert.strictEqual(ahorcado.frase(), '*******')
+      assert.strictEqual(ahorcado.frase(), '¿*** *****!')
     )
     it('horca', () =>
       assert.strictEqual(ahorcado.horca(), 0)
@@ -24,7 +24,7 @@ describe('Ahorcado', () => {
     })
 
     it('frase', () =>
-      assert.strictEqual(ahorcado.frase(), '*a*a**a')
+      assert.strictEqual(ahorcado.frase(), '¿**a **a**!')
     )
     it('horca', () =>
       assert.strictEqual(ahorcado.horca(), 0)
@@ -37,7 +37,7 @@ describe('Ahorcado', () => {
     })
 
     it('frase', () =>
-      assert.strictEqual(ahorcado.frase(), '*******')
+      assert.strictEqual(ahorcado.frase(), '¿*** *****!')
     )
     it('horca', () =>
       assert.strictEqual(ahorcado.horca(), 1)
@@ -47,11 +47,16 @@ describe('Ahorcado', () => {
   describe('es la frase', () => {
     // TODO: Es case sensitive?
     it('correcta', () =>
-      assert.ok(ahorcado.esLaFrase('Palabra'))
+      assert.ok(ahorcado.esLaFrase('¿Una frase!'))
     )
     it('incorrecta', () =>
       assert.ok(!ahorcado.esLaFrase('Ciruela'))
     )
   })
 
+  it('descubrir la misma letra no hace nada', () => {
+    ahorcado.descubrir('z')
+    ahorcado.descubrir('z')
+    assert.strictEqual(ahorcado.horca(), 1)
+  })
 })
