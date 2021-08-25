f74f7355cfffe5f994ecf3efcd964d4c4d61e1bb
PalumboN <nahuel.palumbo@gmail.com>
2021-08-25 03:55:24 -0300

    Escuchando mensajes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Client, Intents } = require('discord.js')
 const { token } = require('./config.json')
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
 
 const canalDelJuego = '879962742390423585'
 
@@ -9,20 +9,41 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
   client.channels.cache
     .get(canalDelJuego)
-    .send("HOLA")
+    .send("TODO: Hacer la presentación")
 })
 
 let n = 0
 
-client.on('interactionCreate', async interaction => {
-  console.log({ interaction })
-  if (!interaction.isCommand()) return
-  if (interaction.channelId !== canalDelJuego) return
+client.on('messageCreate', async message => {
+  if (message.channelId !== canalDelJuego) return
+  if (message.author.id === client.user.id) return
+  console.log({ message })
+  switch (message.content.toLowerCase()) {
+    case 'ping':
+      n++
+      await message.reply(`Pong! ${n}`)
+      break;
 
-  if (interaction.commandName === 'ping') {
-    n++
-    await interaction.reply(`Pong! ${n}`)
+    default:
+      await message.reply({
+        embeds: [{
+          color: 15158332,
+          title: `No entiendo el mensaje '${message.content}'.`
+        }]
+      });
+      break;
   }
 })
+
+// client.on('interactionCreate', async interaction => {
+//   console.log({ interaction })
+//   if (!interaction.isCommand()) return
+//   if (interaction.channelId !== canalDelJuego) return
+
+//   if (interaction.commandName === 'ping') {
+//     n++
+//     await interaction.reply(`Pong! ${n}`)
+//   }
+// })
 
 client.login(token)
